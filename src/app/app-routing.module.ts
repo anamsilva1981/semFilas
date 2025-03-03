13e36b39538f23f1b8d77ada7eeabfd6e87e3947
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-02 22:06:03 -0300

    Add  roteamento para as páginas principais do aplicativo, incluindo restaurantes, reserva, login e gerenciamento de usuários.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestaurantesComponent } from './pages/restaurantes/restaurantes.component';
+import { DetalhesRestauranteComponent } from './pages/detalhes-restaurante/detalhes-restaurante.component';
+import { ReservaComponent } from './pages/reserva/reserva.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { ConfirmacaoComponent } from './pages/confirmacao/confirmacao.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MinhasReservasComponent } from './pages/minhas-reservas/minhas-reservas.component';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: '/restaurantes', pathMatch: 'full' }, // Redireciona pra lista de restaurantes
+  { path: 'restaurantes', component: RestaurantesComponent },
+  { path: 'restaurantes/:id', component: DetalhesRestauranteComponent }, // Usa parâmetro de rota (id)
+  { path: 'reserva/:id', component: ReservaComponent }, // Passa o id do restaurante
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'confirmacao', component: ConfirmacaoComponent },
+  { path: 'minhas-reservas', component: MinhasReservasComponent }
 ];
 
 @NgModule({
