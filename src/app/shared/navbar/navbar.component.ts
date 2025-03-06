fc5e99788fcbfde26971c4603068b5a8c426049d
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-06 00:26:00 -0300

    Substitui conteúdo do Navbar por um Toolbar com título e ícone, e adiciona módulos do PrimeNG

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+
+
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
+    imports: [ToolbarModule, ButtonModule, SplitButtonModule],
     standalone: true
 })
 export class NavbarComponent {
