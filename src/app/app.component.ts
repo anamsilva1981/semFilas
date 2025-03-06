598a640e9012024d551451c7fcd0cda375cf5024
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-05 23:24:30 -0300

    Refatora AppComponent para ser standalone, adiciona Navbar e Footer, e remove AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { FooterComponent } from './shared/footer/footer.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [RouterLink, RouterOutlet, NavbarComponent, FooterComponent, ButtonModule],
 })
 export class AppComponent implements OnInit {
   constructor(private primeng: PrimeNGConfig) {}
