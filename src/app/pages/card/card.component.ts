510ec7cae2abada3024f4382368ff5ced9f31553
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-09 15:53:48 -0300

    Ajustes

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -2,16 +2,31 @@ import { TagModule } from 'primeng/tag';
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-
-
+import { Restaurantes } from '../../interfaces/restaurant.interface';
+import { RestaurantesService } from 'src/app/services/restaurantes.service';
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss'],
-    imports: [CardModule, TagModule, ButtonModule],
-    standalone: true
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  imports: [CardModule, TagModule, ButtonModule, NgFor, HttpClientModule],
+  standalone: true,
 })
 export class CardComponent {
+  constructor(private restaurantesService: RestaurantesService) {}
 
+  public restaurantes: Restaurantes[] = [];
+
+  ngOnInit(): void {
+    this.restaurantesService.getAllRestaurantes().subscribe(
+      (data) => {
+        this.restaurantes = data;
+      },
+      (error) => {
+        console.error('Erro ao carregar os restaurantes', error);
+      }
+    );
+  }
 }
